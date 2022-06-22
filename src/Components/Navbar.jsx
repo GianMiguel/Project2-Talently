5a1ac1f8db89d6a70594678f181cf3f4a13d91f0
Argel Miralles <argelmiralles25@gmail.com>
2022-06-22 17:43:41 +0800

    added showForm functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,17 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    
-
     <div className="header">
       <nav>
-        <h1>talently<span>.</span></h1>
+        <h1>
+          talently<span>.</span>
+        </h1>
         <ul>
-          <li><Link to="/" className="nav--link">Home </Link></li>
-          <li><Link to="/about" className="nav--link">About Us</Link></li>
-          <li><Link to="/talents" className="nav--link">Talents</Link></li>
+          <li>
+            <Link to="/" className="nav--link">
+              Home{" "}
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav--link">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/talents" className="nav--link">
+              Talents
+            </Link>
+          </li>
           <li>Sign In</li>
-          <li><button><Link to="/signup">Sign Up</Link></button></li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
         </ul>
       </nav>
     </div>
---
+++ src/Components/TalentForm.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function talentForm() {
+  return <div>Enter Talent Form Here</div>;
+}
